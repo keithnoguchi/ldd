1cacefadccf4dbf170221aeda9b6660376721cef
Kei Nohguchi <kei@nohguchi.com>
2018-11-19 09:40:32 -0800

    main.c: Register top level ldd bus type and the ldd0 bus device

--- main.c
+++ main.c
@@ -2,10 +2,22 @@
 #include <linux/types.h>
 #include <linux/init.h>
 #include <linux/module.h>
+#include <linux/device.h>
+
+/* ldd_bus_type is the top level virtual bus which hosts
+ * all the ldd devices. */
+static struct bus_type ldd_bus_type = {
+	.name		= "ldd",
+};
 
 static int ldd_init(void)
 {
+	int err;
+
 	printk(KERN_INFO "Welcome to the wonderful kernel world!\n");
+	err = bus_register(&ldd_bus_type);
+	if (err < 0)
+		return err;
 	return 0;
 }
 module_init(ldd_init);
@@ -13,6 +25,7 @@ module_init(ldd_init);
 static void ldd_exit(void)
 {
 	printk(KERN_INFO "Have a wonderful day!\n");
+	bus_unregister(&ldd_bus_type);
 }
 module_exit(ldd_exit);
 
