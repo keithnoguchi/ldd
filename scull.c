32af184272d921cbcf19c155858f981b5d2789cf
Kei Nohguchi <kei@nohguchi.com>
2018-11-26 15:26:26 -0800

    scull: Add scull_qset to manage the quantum
    The driver is not working, due to the missing functions, both
    for read and write.

--- scull.c
+++ scull.c
@@ -24,7 +24,7 @@ static struct scull_device {
 	struct semaphore	sem;
 	size_t			size;
 	size_t			bufsiz;
-	void			*data;
+	struct scull_qset	*data;
 	struct device		dev;
 	struct cdev		cdev;
 } devices[] = {
@@ -33,6 +33,47 @@ static struct scull_device {
 	},
 	{},	/* sentry */
 };
+
+/* Scull quantum set */
+struct scull_qset {
+	struct scull_qset	*next;
+	void			**data;
+};
+
+/* trim all the quantum data.  The device should be locked. */
+static void scull_trim(struct scull_device *d)
+{
+	struct scull_driver *drv = container_of(d->dev.driver, struct scull_driver, drv);
+	struct scull_qset *qset, *next;
+	int i;
+
+	for (qset = d->data; qset; qset = next) {
+		next = qset->next;
+		if (qset->data) {
+			for (i = 0; i < drv->qset; i++)
+				kfree(qset->data[i]);
+			kfree(qset->data);
+		}
+		kfree(qset);
+	}
+	d->data = NULL;
+	d->bufsiz = 0;
+	d->size = 0;
+}
+
+/* get the quantum for read operation.  The device should be locked. */
+static void *scull_quantum(struct scull_device *d, loff_t pos)
+{
+	/* TBD */
+	return NULL;
+}
+
+/* get the quantum for write operation.  The device should be locked. */
+static void *scull_get_quantum(struct scull_device *d, loff_t pos)
+{
+	/* TBD */
+	return NULL;
+}
 
 /* Scull device attributes */
 static ssize_t pagesize_show(struct device *dev, struct device_attribute *attr,
@@ -148,16 +189,27 @@ static loff_t scull_llseek(struct file *f, loff_t offset, int whence)
 static ssize_t scull_read(struct file *f, char __user *buf, size_t len, loff_t *pos)
 {
 	struct scull_device *d = f->private_data;
+	struct scull_driver *drv = container_of(d->dev.driver, struct scull_driver, drv);
+	void *qptr;
+	loff_t qpos;
 	ssize_t ret;
 
 	if (down_interruptible(&d->sem))
 		return -ERESTARTSYS;
 	if (*pos+len > d->size)
 		len = d->size-*pos;
+	ret = -EINVAL;
+	qptr = scull_quantum(d, *pos);
+	if (qptr)
+		goto out;
+	/* support per quantum read only */
+	qpos = *pos%drv->quantum;
+	if (qpos+len > drv->quantum)
+		len = drv->quantum-qpos;
 	ret = 0;
 	if (len == 0)
 		goto out;
-	ret = copy_to_user(buf, d->data+*pos, len);
+	ret = copy_to_user(buf, qptr+qpos, len);
 	if (unlikely(ret))
 		len -= ret;
 	*pos += len;
@@ -170,31 +222,32 @@ out:
 static ssize_t scull_write(struct file *f, const char __user *buf, size_t len, loff_t *pos)
 {
 	struct scull_device *d = f->private_data;
+	struct scull_driver *drv = container_of(d->dev.driver, struct scull_driver, drv);
+	void *qptr;
+	loff_t qpos;
 	ssize_t ret;
 
 	if (down_interruptible(&d->sem))
 		return -ERESTARTSYS;
-	if (*pos+len > d->size) {
-		/* as simple as this for now */
-		void *prev = d->data;
-		d->data = kmalloc(*pos+len, GFP_KERNEL);
-		if (d->data == NULL) {
-			ret = -ENOMEM;
-			kfree(prev);
-			goto out;
-		}
-		memcpy(d->data, prev, d->bufsiz);
-		d->bufsiz = *pos+len;
-		d->size = d->bufsiz;
-	}
+	/* find the quantum */
+	ret = -ENOMEM;
+	qptr = scull_get_quantum(d, *pos);
+	if (qptr == NULL)
+		goto out;
+	/* support per quantum write only */
+	qpos = *pos%drv->quantum;
+	if (qpos+len > drv->quantum)
+		len = drv->quantum-qpos;
 	ret = 0;
 	if (len == 0)
 		goto out;
-	ret = copy_from_user(d->data+*pos, buf, len);
+	ret = copy_from_user(qptr+qpos, buf, len);
 	if (unlikely(ret))
 		len -= ret;
 	ret = len;
 	*pos += len;
+	if (*pos > d->size)
+		d->size = *pos;
 out:
 	up(&d->sem);
 	return ret;
@@ -208,12 +261,8 @@ static int scull_open(struct inode *i, struct file *f)
 	if (down_interruptible(&d->sem))
 		return -ERESTARTSYS;
 	/* reset the file size when it's opened for write only */
-	if ((f->f_flags&O_ACCMODE) == O_WRONLY) {
-		kfree(d->data);
-		d->data = NULL;
-		d->bufsiz = 0;
-		d->size = 0;
-	}
+	if ((f->f_flags&O_ACCMODE) == O_WRONLY)
+		scull_trim(d);
 	up(&d->sem);
 	return 0;
 }
@@ -270,9 +319,10 @@ out:
 void scull_unregister(void)
 {
 	struct scull_device *d;
+
 	for (d = devices; dev_name(&d->dev); d++) {
 		cdev_device_del(&d->cdev, &d->dev);
-		kfree(d->data);
+		scull_trim(d);
 	}
 	unregister_chrdev_region(devices[0].dev.devt, ARRAY_SIZE(devices));
 }
