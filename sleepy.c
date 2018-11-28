03d6ea0f8986c094754e22aac95f978f2894b068
Kei Nohguchi <kei@nohguchi.com>
2018-11-28 11:27:41 -0800

    sleepy: Use the standard misc_open() to setup the f->private_data
    read, write, and release methods are good enough for misc device
    up and running.

--- sleepy.c
+++ sleepy.c
@@ -1,33 +1,38 @@
 /* SPDX-License-Identifier: GPL-2.0 */
 #include <linux/types.h>
+#include <linux/kernel.h>
+#include <linux/device.h>
 #include <linux/miscdevice.h>
 #include <linux/fs.h>
 
 /* sleep device file operations */
 static ssize_t sleepy_read(struct file *f, char __user *buf, size_t nr, loff_t *pos)
 {
+	struct miscdevice *m = f->private_data;
+	struct device *dev = m->this_device;
+	printk(KERN_INFO "read(%s)\n", dev_name(dev));
 	return 0;
 }
 
 static ssize_t sleepy_write(struct file *f, const char __user *buf, size_t nr, loff_t *pos)
 {
-	return 0;
-}
-
-static int sleepy_open(struct inode *i, struct file *f)
-{
+	struct miscdevice *m = f->private_data;
+	struct device *dev = m->this_device;
+	printk(KERN_INFO "write(%s)\n", dev_name(dev));
 	return 0;
 }
 
 static int sleepy_release(struct inode *i, struct file *f)
 {
+	struct miscdevice *m = f->private_data;
+	struct device *dev = m->this_device;
+	printk(KERN_INFO "release(%s)\n", dev_name(dev));
 	return 0;
 }
 
 static const struct file_operations sleepy_fops = {
 	.read		= sleepy_read,
 	.write		= sleepy_write,
-	.open		= sleepy_open,
 	.release	= sleepy_release,
 };
 
