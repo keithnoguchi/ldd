fafa99a573019886e890a1aa9b9ca328acba19ee
Kei Nohguchi <kei@nohguchi.com>
2019-04-03 23:01:58 -0700

    scull: Delete obsolete header files

--- scull.h
+++
@@ -1,8 +0,0 @@
-/* SPDX-License-Identifier: GPL-2.0 */
-#ifndef _DRIVER_LDD_SCULL_H
-#define _DRIVER_LDD_SCULL_H
-
-int __init scull_register(void);
-void scull_unregister(void);
-
-#endif /* _DRIVER_LDD_SCULL_H */
--- sculld.h
+++
@@ -1,8 +0,0 @@
-/* SPDX-License-Identifier: GPL-2.0 */
-#ifndef _DRIVER_LDD_SCULLD_H
-#define _DRIVER_LDD_SCULLD_H
-
-int __init sculld_register(void);
-void sculld_unregister(void);
-
-#endif /* _DRIVER_LDD_SCULLD_H */
