a5fc776376d1ff56744d281f71796ab5b7b15f45
Kei Nohguchi <kei@nohguchi.com>
2019-04-24 16:37:14 -0700

    seq: Create /proc/driver/seq directory

--- seq.c
+++ seq.c
@@ -1,19 +1,43 @@
 /* SPDX-License-Identifier: GPL-2.0 */
 #include <linux/init.h>
 #include <linux/module.h>
+#include <linux/string.h>
 #include <linux/kernel.h>
+#include <linux/device.h>
+#include <linux/proc_fs.h>
 #include <linux/seq_file.h>
+
+static struct seq_driver {
+	struct proc_dir_entry	*top;
+	struct device_driver	base;
+} seq_driver = {
+	.top		= NULL,
+	.base.name	= "seq",
+	.base.owner	= THIS_MODULE,
+};
 
 static int __init init(void)
 {
-	printk(KERN_ALERT "here you go!\n");
+	struct seq_driver *drv = &seq_driver;
+	struct proc_dir_entry *dir;
+	char path[11];
+	int err;
+
+	err = snprintf(path, sizeof(path), "driver/%s", drv->base.name);
+	if (err < 0)
+		return -1;
+	dir = proc_mkdir(path, NULL);
+	if (IS_ERR(dir))
+		return PTR_ERR(dir);
+	drv->top = dir;
 	return 0;
 }
 module_init(init);
 
 static void __exit term(void)
 {
-	printk(KERN_ALERT "goodbye\n");
+	struct seq_driver *drv = &seq_driver;
+	proc_remove(drv->top);
 }
 module_exit(term);
 
--- tests/seq_test.c
+++ tests/seq_test.c
@@ -1,9 +1,78 @@
 /* SPDX-License-Identifier: GPL-2.0 */
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
 #include "kselftest.h"
+
+struct test {
+	const char	*const name;
+	const char	*const path;
+	int		flags;
+};
+
+static void test(const struct test *restrict t)
+{
+	int fd;
+
+	fd = open(t->path, t->flags);
+	if (fd == -1) {
+		perror("open");
+		goto err;
+	}
+	exit(EXIT_SUCCESS);
+err:
+	exit(EXIT_FAILURE);
+}
 
 int main(void)
 {
+	const struct test *t, tests[] = {
+		{
+			.name	= "/proc/driver/seq directory",
+			.path	= "/proc/driver/seq",
+			.flags	= O_RDONLY|O_DIRECTORY,
+		},
+		{.name = NULL}, /* sentry */
+	};
+
+	for (t = tests; t->name; t++) {
+		int ret, status;
+		pid_t pid;
+
+		pid = fork();
+		if (pid == -1) {
+			perror("fork");
+			goto err;
+		} else if (pid == 0)
+			test(t);
+		ret = waitpid(pid, &status, 0);
+		if (ret == -1) {
+			perror("waitpid");
+			goto err;
+		}
+		if (WIFSIGNALED(status)) {
+			fprintf(stderr, "%s: child signaled with %s\n",
+				t->name, strsignal(WTERMSIG(status)));
+			goto err;
+		}
+		if (!WIFEXITED(status)) {
+			fprintf(stderr, "%s: child does not exit\n",
+				t->name);
+			goto err;
+		}
+		if (WEXITSTATUS(status))
+			goto err;
+		ksft_inc_pass_cnt();
+		continue;
+err:
+		ksft_inc_fail_cnt();
+	}
 	if (ksft_get_fail_cnt())
 		ksft_exit_fail();
 	ksft_exit_pass();
