6bdd401923d2a8e453d8630c077c7dc5625e8891
Kei Nohguchi <kei@nohguchi.com>
2019-05-06 23:46:00 -0700

    append: Add the buffer contents camparison test

--- tests/append_test.c
+++ tests/append_test.c
@@ -5,6 +5,7 @@
 #include <limits.h>
 #include <fcntl.h>
 #include <errno.h>
+#include <ctype.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
@@ -15,31 +16,61 @@ struct test {
 	const char	*const dev;
 	int		flags;
 	int		nr;
+	char		mark[5];
 	size_t		size;
 	size_t		want;
 	size_t		alloc;
 };
 
+static void dump(FILE *s, const char *tag, const unsigned char *restrict buf, size_t len)
+{
+	int i, j, width = 16;
+
+	fprintf(s, "%s> ", tag);
+	for (i = 0; i < len; i++) {
+		fprintf(s, "%02x ", buf[i]);
+		/* check if we are done for this line */
+		if (i%width == (width-1) || i+1 == len) {
+			/* fill the gap */
+			for (j = (i%width); j < (width-1); j++)
+				fprintf(s, "   ");
+			/* ascii print */
+			fprintf(s, "| ");
+			for (j = i-(i%width); j <= i; j++)
+				fprintf(s, "%c", isprint(buf[j]) ? buf[j] : '.');
+			fprintf(s, "\n%s> ", tag);
+		}
+	}
+	fprintf(s, "\n");
+}
+
+
 static void test(const struct test *restrict t)
 {
+	char *ptr, obuf[t->size], ibuf[t->size];
 	char path[PATH_MAX];
-	char *ptr, buf[t->size];
-	ssize_t len, got;
+	ssize_t len, rem, got;
 	int i, err, fd;
 	FILE *fp;
 
 	err = snprintf(path, sizeof(path), "/dev/%s", t->dev);
 	if (err < 0)
 		goto perr;
+	ptr = obuf;
+	for (i = 0; i < t->size/sizeof(t->mark); i++) {
+		memcpy(ptr, t->mark, sizeof(t->mark));
+		ptr += sizeof(t->mark);
+	}
+	for (i = 0; i < t->size%sizeof(t->mark); i++)
+		ptr[i] = t->mark[i];
 	for (i = 0; i < t->nr; i++) {
 		int flags = t->flags|O_WRONLY;
-		int rem;
 
 		fd = open(path, i == 0 ? flags|O_TRUNC : flags);
 		if (fd == -1)
 			goto perr;
 		rem = t->size;
-		ptr = buf;
+		ptr = obuf;
 		do {
 			len = write(fd, ptr, rem);
 			if (len == -1) {
@@ -56,9 +87,25 @@ static void test(const struct test *restrict t)
 	fd = open(path, O_RDONLY);
 	if (fd == -1)
 		goto perr;
+	rem = sizeof(ibuf);
+	ptr = ibuf;
 	got = 0;
-	while ((len = read(fd, buf, sizeof(buf))) > 0)
+	while ((len = read(fd, ptr, rem)) > 0) {
 		got += len;
+		rem -= len;
+		ptr += len;
+		if (rem == 0) {
+			if (memcmp(ibuf, obuf, sizeof(ibuf))) {
+				fprintf(stderr, "%s: unexpected receive data\n",
+					t->name);
+				dump(stderr, "ibuf", (unsigned char *)ibuf, sizeof(ibuf));
+				dump(stderr, "obuf", (unsigned char *)obuf, sizeof(ibuf));
+				goto err;
+			}
+			rem = sizeof(ibuf);
+			ptr = ibuf;
+		}
+	}
 	if (close(fd) == -1)
 		goto perr;
 	if (got != t->want) {
@@ -72,12 +119,12 @@ static void test(const struct test *restrict t)
 	fp = fopen(path, "r");
 	if (!fp)
 		goto perr;
-	err = fread(buf, sizeof(buf), 1, fp);
+	err = fread(ibuf, sizeof(ibuf), 1, fp);
 	if (err == 0 && ferror(fp))
 		goto perr;
 	if (fclose(fp) == -1)
 		goto perr;
-	got = strtol(buf, NULL, 10);
+	got = strtol(ibuf, NULL, 10);
 	if (got != t->want) {
 		fprintf(stderr, "%s: unexpected %s value:\n\t- want: %ld\n\t-  got: %ld\n",
 			t->name, path, t->want, got);
@@ -89,10 +136,10 @@ static void test(const struct test *restrict t)
 	fp = fopen(path, "r");
 	if (!fp)
 		goto perr;
-	err = fread(buf, sizeof(buf), 1, fp);
+	err = fread(ibuf, sizeof(ibuf), 1, fp);
 	if (err == 0 && ferror(fp))
 		goto perr;
-	got = strtol(buf, NULL, 10);
+	got = strtol(ibuf, NULL, 10);
 	if (got != t->alloc) {
 		fprintf(stderr, "%s: unexpected %s value:\n\t- want: %ld\n\t-  got: %ld\n",
 			t->name, path, t->alloc, got);
@@ -113,6 +160,7 @@ int main(void)
 			.name	= "signle 4095 write no O_APPEND flag",
 			.dev	= "append0",
 			.flags	= 0,
+			.mark	= {0x01, 0xde, 0xad, 0xbe, 0xef},
 			.nr	= 1,
 			.size	= 4095,
 			.want	= 4095,
@@ -122,6 +170,7 @@ int main(void)
 			.name	= "single 4095 write with O_APPEND flag",
 			.dev	= "append1",
 			.flags	= O_APPEND,
+			.mark	= {0x02, 0xde, 0xad, 0xbe, 0xef},
 			.nr	= 1,
 			.size	= 4095,
 			.want	= 4095,
@@ -131,6 +180,7 @@ int main(void)
 			.name	= "signle 4096 write no O_APPEND flag",
 			.dev	= "append2",
 			.flags	= 0,
+			.mark	= {0x03, 0xde, 0xad, 0xbe, 0xef},
 			.nr	= 1,
 			.size	= 4096,
 			.want	= 4096,
@@ -140,6 +190,7 @@ int main(void)
 			.name	= "single 4096 write with O_APPEND flag",
 			.dev	= "append3",
 			.flags	= O_APPEND,
+			.mark	= {0x04, 0xde, 0xad, 0xbe, 0xef},
 			.nr	= 1,
 			.size	= 4096,
 			.want	= 4096,
@@ -149,6 +200,7 @@ int main(void)
 			.name	= "signle 4097 write no O_APPEND flag",
 			.dev	= "append0",
 			.flags	= 0,
+			.mark	= {0x05, 0xde, 0xad, 0xbe, 0xef},
 			.nr	= 1,
 			.size	= 4097,
 			.want	= 4097,
@@ -158,6 +210,7 @@ int main(void)
 			.name	= "single 4097 write with O_APPEND flag",
 			.dev	= "append1",
 			.flags	= O_APPEND,
+			.mark	= {0x06, 0xde, 0xad, 0xbe, 0xef},
 			.nr	= 1,
 			.size	= 4097,
 			.want	= 4097,
@@ -167,6 +220,7 @@ int main(void)
 			.name	= "double 4095 writes no O_APPEND flag",
 			.dev	= "append2",
 			.flags	= 0,
+			.mark	= {0x07, 0xde, 0xad, 0xbe, 0xef},
 			.nr	= 2,
 			.size	= 4095,
 			.want	= 4095,
@@ -176,6 +230,7 @@ int main(void)
 			.name	= "double 4095 writes with O_APPEND flag",
 			.dev	= "append3",
 			.flags	= O_APPEND,
+			.mark	= {0x08, 0xde, 0xad, 0xbe, 0xef},
 			.nr	= 2,
 			.size	= 4095,
 			.want	= 8190,
@@ -185,6 +240,7 @@ int main(void)
 			.name	= "double 4096 writes no O_APPEND flag",
 			.dev	= "append0",
 			.flags	= 0,
+			.mark	= {0x09, 0xde, 0xad, 0xbe, 0xef},
 			.nr	= 2,
 			.size	= 4096,
 			.want	= 4096,
@@ -194,6 +250,7 @@ int main(void)
 			.name	= "double 4096 writes with O_APPEND flag",
 			.dev	= "append1",
 			.flags	= O_APPEND,
+			.mark	= {0x0a, 0xde, 0xad, 0xbe, 0xef},
 			.nr	= 2,
 			.size	= 4096,
 			.want	= 8192,
@@ -203,6 +260,7 @@ int main(void)
 			.name	= "double 4097 writes no O_APPEND flag",
 			.dev	= "append2",
 			.flags	= 0,
+			.mark	= {0x0b, 0xde, 0xad, 0xbe, 0xef},
 			.nr	= 2,
 			.size	= 4097,
 			.want	= 4097,
@@ -212,6 +270,7 @@ int main(void)
 			.name	= "double 4097 writes with O_APPEND flag",
 			.dev	= "append3",
 			.flags	= O_APPEND,
+			.mark	= {0x0c, 0xde, 0xad, 0xbe, 0xef},
 			.nr	= 2,
 			.size	= 4097,
 			.want	= 8194,
@@ -221,6 +280,7 @@ int main(void)
 			.name	= "triple 4095 writes no O_APPEND flag",
 			.dev	= "append0",
 			.flags	= 0,
+			.mark	= {0x0d, 0xde, 0xad, 0xbe, 0xef},
 			.nr	= 3,
 			.size	= 4095,
 			.want	= 4095,
@@ -230,6 +290,7 @@ int main(void)
 			.name	= "triple 4095 writes with O_APPEND flag",
 			.dev	= "append1",
 			.flags	= O_APPEND,
+			.mark	= {0x0e, 0xde, 0xad, 0xbe, 0xef},
 			.nr	= 3,
 			.size	= 4095,
 			.want	= 12285,
@@ -239,6 +300,7 @@ int main(void)
 			.name	= "triple 4096 writes no O_APPEND flag",
 			.dev	= "append2",
 			.flags	= 0,
+			.mark	= {0x0f, 0xde, 0xad, 0xbe, 0xef},
 			.nr	= 3,
 			.size	= 4096,
 			.want	= 4096,
@@ -248,6 +310,7 @@ int main(void)
 			.name	= "triple 4096 writes with O_APPEND flag",
 			.dev	= "append3",
 			.flags	= O_APPEND,
+			.mark	= {0x10, 0xde, 0xad, 0xbe, 0xef},
 			.nr	= 3,
 			.size	= 4096,
 			.want	= 12288,
@@ -257,6 +320,7 @@ int main(void)
 			.name	= "triple 4097 writes no O_APPEND flag",
 			.dev	= "append0",
 			.flags	= 0,
+			.mark	= {0x11, 0xde, 0xad, 0xbe, 0xef},
 			.nr	= 3,
 			.size	= 4097,
 			.want	= 4097,
@@ -266,6 +330,7 @@ int main(void)
 			.name	= "triple 4097 writes with O_APPEND flag",
 			.dev	= "append1",
 			.flags	= O_APPEND,
+			.mark	= {0x12, 0xde, 0xad, 0xbe, 0xef},
 			.nr	= 3,
 			.size	= 4097,
 			.want	= 12291,
