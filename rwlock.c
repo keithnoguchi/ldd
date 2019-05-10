6b485b1159abda5c3a69d725989182d51b24dfc0
Kei Nohguchi <kei@nohguchi.com>
2019-05-09 20:58:56 -0700

    rwlock: Create /dev/rwlockX device nodes

--- rwlock.c
+++ rwlock.c
@@ -3,22 +3,101 @@
 #include <linux/types.h>
 #include <linux/module.h>
 #include <linux/kernel.h>
+#include <linux/string.h>
+#include <linux/fs.h>
+#include <linux/sysfs.h>
+#include <linux/device.h>
+#include <linux/miscdevice.h>
+#include <linux/spinlock.h>
+
+struct rwlock_context {
+	struct rwlock_context	*next;
+	struct file		*fp;
+};
+
+struct rwlock_device {
+	rwlock_t		lock;
+	struct rwlock_context	*head;
+	struct rwlock_context	*free;
+	struct miscdevice	base;
+};
+
+static struct rwlock_driver {
+	struct file_operations	fops;
+	struct device_driver	base;
+	struct rwlock_device	devs[2];
+} rwlock_driver = {
+	.base.name	= "rwlock",
+	.base.owner	= THIS_MODULE,
+};
+
+static int open(struct inode *ip, struct file *fp)
+{
+	return 0;
+}
+
+static int release(struct inode *ip, struct file *fp)
+{
+	return 0;
+}
+
+static int __init init_driver(struct rwlock_driver *drv)
+{
+	memset(&drv->fops, 0, sizeof(struct file_operations));
+	drv->fops.owner		= drv->base.owner;
+	drv->fops.open		= open;
+	drv->fops.release	= release;
+	return 0;
+}
 
 static int __init init(void)
 {
+	struct rwlock_driver *drv = &rwlock_driver;
+	struct rwlock_device *end = drv->devs+ARRAY_SIZE(drv->devs);
+	struct rwlock_device *dev;
+	char name[8]; /* strlen(drv->base.name)+2 */
+	int i, err;
+
+	err = init_driver(drv);
+	if (err)
+		return err;
+	for (dev = drv->devs, i = 0; dev != end; dev++, i++) {
+		err = snprintf(name, sizeof(name), "%s%d", drv->base.name, i);
+		if (err < 0) {
+			end = dev;
+			goto err;
+		}
+		memset(dev, 0, sizeof(struct rwlock_device));
+		rwlock_init(&dev->lock);
+		dev->head = dev->free	= NULL;
+		dev->base.name		= name;
+		dev->base.fops		= &drv->fops;
+		dev->base.minor		= MISC_DYNAMIC_MINOR;
+		err = misc_register(&dev->base);
+		if (err) {
+			end = dev;
+			goto err;
+		}
+	}
 	return 0;
+err:
+	for (dev = drv->devs; dev != end; dev++)
+		misc_deregister(&dev->base);
+	return err;
 }
 module_init(init);
 
 static void __exit term(void)
 {
-	return;
+	struct rwlock_driver *drv = &rwlock_driver;
+	struct rwlock_device *end = drv->devs+ARRAY_SIZE(drv->devs);
+	struct rwlock_device *dev;
+
+	for (dev = drv->devs; dev != end; dev++)
+		misc_deregister(&dev->base);
 }
 module_exit(term);
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Kei Nohguchi <kei@nohguchi.com>");
 MODULE_DESCRIPTION("R/W spin lock test module");
-
-
-
--- tests/rwlock_test.c
+++ tests/rwlock_test.c
@@ -2,8 +2,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <limits.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <pthread.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include "kselftest.h"
@@ -11,16 +13,115 @@
 struct test {
 	const char	*const name;
 	const char	*const dev;
+	unsigned int	nr;
+	pthread_mutex_t	lock;
+	pthread_cond_t	cond;
+	int		start;
 };
 
 static void test(const struct test *restrict t)
 {
+	char path[PATH_MAX];
+	int fd[t->nr];
+	int i, err;
+
+	err = snprintf(path, sizeof(path), "/dev/%s", t->dev);
+	if (err < 0)
+		goto perr;
+	memset(fd, 0, sizeof(fd));
+	for (i = 0; i < t->nr; i++) {
+		fd[i] = open(path, O_RDONLY);
+		if (fd[i] == -1)
+			goto perr;
+	}
+	pthread_yield();
+	for (i = 0; i < t->nr; i++) {
+		if (!fd[i])
+			continue;
+		if (close(fd[i]) == -1)
+			goto perr;
+	}
 	exit(EXIT_SUCCESS);
+perr:
+	perror(t->name);
+	exit(EXIT_FAILURE);
 }
 
 int main(void)
 {
 	const struct test *t, tests[] = {
+		{
+			.name	= "single thread",
+			.dev	= "rwlock0",
+			.nr	= 1,
+			.lock	= PTHREAD_MUTEX_INITIALIZER,
+			.cond	= PTHREAD_COND_INITIALIZER,
+			.start	= 0,
+		},
+		{
+			.name	= "double threads",
+			.dev	= "rwlock1",
+			.nr	= 2,
+			.lock	= PTHREAD_MUTEX_INITIALIZER,
+			.cond	= PTHREAD_COND_INITIALIZER,
+			.start	= 0,
+		},
+		{
+			.name	= "triple threads",
+			.dev	= "rwlock0",
+			.nr	= 3,
+			.lock	= PTHREAD_MUTEX_INITIALIZER,
+			.cond	= PTHREAD_COND_INITIALIZER,
+			.start	= 0,
+		},
+		{
+			.name	= "quad threads",
+			.dev	= "rwlock1",
+			.nr	= 4,
+			.lock	= PTHREAD_MUTEX_INITIALIZER,
+			.cond	= PTHREAD_COND_INITIALIZER,
+			.start	= 0,
+		},
+		{
+			.name	= "32 threads",
+			.dev	= "rwlock0",
+			.nr	= 32,
+			.lock	= PTHREAD_MUTEX_INITIALIZER,
+			.cond	= PTHREAD_COND_INITIALIZER,
+			.start	= 0,
+		},
+		{
+			.name	= "64 threads",
+			.dev	= "rwlock1",
+			.nr	= 64,
+			.lock	= PTHREAD_MUTEX_INITIALIZER,
+			.cond	= PTHREAD_COND_INITIALIZER,
+			.start	= 0,
+		},
+		{
+			.name	= "128 threads",
+			.dev	= "rwlock0",
+			.nr	= 128,
+			.lock	= PTHREAD_MUTEX_INITIALIZER,
+			.cond	= PTHREAD_COND_INITIALIZER,
+			.start	= 0,
+		},
+		{
+			.name	= "256 threads",
+			.dev	= "rwlock1",
+			.nr	= 256,
+			.lock	= PTHREAD_MUTEX_INITIALIZER,
+			.cond	= PTHREAD_COND_INITIALIZER,
+			.start	= 0,
+		},
+		{
+			.name	= "512 threads",
+			.dev	= "rwlock0",
+			.nr	= 512,
+			.lock	= PTHREAD_MUTEX_INITIALIZER,
+			.cond	= PTHREAD_COND_INITIALIZER,
+			.start	= 0,
+		},
 		{.name = NULL},
 	};
 
