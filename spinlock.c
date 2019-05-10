6cd20be9c6cd7dc1ec5a9ae6e2627b91abb08f65
Kei Nohguchi <kei@nohguchi.com>
2019-05-09 20:36:42 -0700

    spinlock: Use free list for the context allocation management

--- spinlock.c
+++ spinlock.c
@@ -20,6 +20,7 @@ struct spinlock_context {
 struct spinlock_device {
 	spinlock_t		lock;
 	struct spinlock_context	*head;
+	struct spinlock_context	*free;
 	struct miscdevice	base;
 };
 
@@ -37,15 +38,22 @@ static int open(struct inode *ip, struct file *fp)
 	struct spinlock_device *dev = container_of(fp->private_data,
 						   struct spinlock_device,
 						   base);
-	struct spinlock_context **ctx, *tmp = NULL;
+	struct spinlock_context **ctx, *tmp;
 
-	/* allocate the context just in case it's brand new */
-	tmp = kzalloc(sizeof(struct spinlock_context), GFP_KERNEL);
-	if (IS_ERR(tmp))
-		return PTR_ERR(tmp);
+	/* get the context from the free list if there is */
+	spin_lock(&dev->lock);
+	tmp = dev->free;
+	if (tmp)
+		dev->free = tmp->next;
+	spin_unlock(&dev->lock);
+	if (!tmp) {
+		tmp = kzalloc(sizeof(struct spinlock_context), GFP_KERNEL);
+		if (IS_ERR(tmp))
+			return PTR_ERR(tmp);
+	}
+	tmp->next = NULL;
 	tmp->count = 1;
 	tmp->fp = fp;
-
 	/* just make counter up if there is a context already */
 	spin_lock(&dev->lock);
 	for (ctx = &dev->head; *ctx; ctx = &(*ctx)->next)
@@ -57,9 +65,11 @@ static int open(struct inode *ip, struct file *fp)
 	*ctx = tmp;
 	tmp = NULL;
 out:
+	if (tmp) {
+		tmp->next = dev->free;
+		dev->free = tmp;
+	}
 	spin_unlock(&dev->lock);
-	if (tmp)
-		kfree(tmp);
 	return 0;
 }
 
@@ -76,7 +86,8 @@ static int release(struct inode *ip, struct file *fp)
 			got = *ctx;
 			if (--(*ctx)->count == 0) {
 				*ctx = (*ctx)->next;
-				kfree(got);
+				got->next = dev->free;
+				dev->free = got;
 			}
 			goto out;
 		}
@@ -105,8 +116,27 @@ static ssize_t active_show(struct device *base,
 }
 static DEVICE_ATTR_RO(active);
 
+static ssize_t free_show(struct device *base,
+			 struct device_attribute *attr,
+			 char *page)
+{
+	struct spinlock_device *dev = container_of(dev_get_drvdata(base),
+						   struct spinlock_device,
+						   base);
+	struct spinlock_context *ctx;
+	size_t nr = 0;
+
+	spin_lock(&dev->lock);
+	for (ctx = dev->free; ctx; ctx = ctx->next)
+		nr++;
+	spin_unlock(&dev->lock);
+	return snprintf(page, PAGE_SIZE, "%ld\n", nr);
+}
+static DEVICE_ATTR_RO(free);
+
 static struct attribute *spinlock_attrs[] = {
 	&dev_attr_active.attr,
+	&dev_attr_free.attr,
 	NULL,
 };
 ATTRIBUTE_GROUPS(spinlock);
@@ -139,7 +169,7 @@ static int __init init(void)
 		}
 		memset(dev, 0, sizeof(struct spinlock_device));
 		spin_lock_init(&dev->lock);
-		dev->head		= NULL;
+		dev->head = dev->free	= NULL;
 		dev->base.name		= name;
 		dev->base.fops		= &drv->fops;
 		dev->base.minor		= MISC_DYNAMIC_MINOR;
@@ -167,6 +197,10 @@ static void __exit term(void)
 	for (dev = drv->devs; dev != end; dev++) {
 		struct spinlock_context *ctx, *next;
 		for (ctx = dev->head; ctx; ctx = next) {
+			next = ctx->next;
+			kfree(ctx);
+		}
+		for (ctx = dev->free; ctx; ctx = next) {
 			next = ctx->next;
 			kfree(ctx);
 		}
