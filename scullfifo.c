eb25202c4cae378152f45625344e0970b3cf9dc0
Kei Nohguchi <kei@nohguchi.com>
2019-06-10 13:46:14 -0700

    scullfifo: Create device nodes under /dev directory

--- scullfifo.c
+++ scullfifo.c
@@ -3,16 +3,98 @@
 #include <linux/types.h>
 #include <linux/module.h>
 #include <linux/kernel.h>
+#include <linux/string.h>
+#include <linux/fs.h>
+#include <linux/cdev.h>
+#include <linux/device.h>
+
+struct scullfifo_device {
+	struct cdev	cdev;
+	struct device	base;
+};
+
+static struct scullfifo_driver {
+	dev_t			devt;
+	struct file_operations	fops;
+	struct device_driver	base;
+	struct scullfifo_device	devs[2];
+} scullfifo_driver = {
+	.base.name	= "scullfifo",
+	.base.owner	= THIS_MODULE,
+};
+
+static int open(struct inode *ip, struct file *fp)
+{
+	return 0;
+}
+
+static int release(struct inode *ip, struct file *fp)
+{
+	return 0;
+}
+
+static int __init init_driver(struct scullfifo_driver *drv)
+{
+	int err;
+
+	err = alloc_chrdev_region(&drv->devt, 0, ARRAY_SIZE(drv->devs),
+				  drv->base.name);
+	if (err)
+		return err;
+	memset(&drv->fops, 0, sizeof(struct file_operations));
+	drv->fops.owner		= drv->base.owner;
+	drv->fops.open		= open;
+	drv->fops.release	= release;
+	return 0;
+}
 
 static int __init init(void)
 {
+	struct scullfifo_driver *drv = &scullfifo_driver;
+	struct scullfifo_device *end = drv->devs+ARRAY_SIZE(drv->devs);
+	struct scullfifo_device *dev;
+	char name[11]; /* strlen(drv->base.name)+2 */
+	int err, i;
+
+	err = init_driver(drv);
+	if (err)
+		return err;
+	for (dev = drv->devs, i = 0; dev != end; dev++, i++) {
+		err = snprintf(name, sizeof(name), "%s%d", drv->base.name, i);
+		if (err < 0) {
+			end = dev;
+			goto err;
+		}
+		memset(dev, 0, sizeof(struct scullfifo_device));
+		device_initialize(&dev->base);
+		cdev_init(&dev->cdev, &drv->fops);
+		dev->base.init_name	= name;
+		dev->base.devt		= MKDEV(MAJOR(drv->devt),
+						MINOR(drv->devt)+i);
+		err = cdev_device_add(&dev->cdev, &dev->base);
+		if (err) {
+			end = dev;
+			goto err;
+		}
+	}
 	return 0;
+err:
+	for (dev = drv->devs; dev != end; dev++)
+		cdev_device_del(&dev->cdev, &dev->base);
+	unregister_chrdev_region(drv->devt, ARRAY_SIZE(drv->devs));
+	return err;
 }
 module_init(init);
 
 static void __exit term(void)
 {
-	return;
+	struct scullfifo_driver *drv = &scullfifo_driver;
+	struct scullfifo_device *end = drv->devs+ARRAY_SIZE(drv->devs);
+	struct scullfifo_device *dev;
+
+	for (dev = drv->devs; dev != end; dev++)
+		cdev_device_del(&dev->cdev, &dev->base);
+	unregister_chrdev_region(drv->devt, ARRAY_SIZE(drv->devs));
 }
 module_exit(term);
 
--- tests/scullfifo_test.c
+++ tests/scullfifo_test.c
@@ -3,7 +3,10 @@
 #include <stdlib.h>
 #include <string.h>
 #include <limits.h>
+#include <fcntl.h>
 #include <errno.h>
+#include <unistd.h>
+#include <pthread.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <sys/resource.h>
@@ -12,16 +15,239 @@
 struct test {
 	const char	*const name;
 	const char	*const dev;
+	unsigned int	readers;
+	unsigned int	writers;
+	size_t		rbufsiz;
+	size_t		wbufsiz;
+	size_t		bufsiz;
+	size_t		alloc;
 };
+
+struct context {
+	const struct test	*const t;
+	pthread_mutex_t		lock;
+	pthread_cond_t		cond;
+	int			start;
+};
+
+static void *tester(struct context *ctx, int flags)
+{
+	const struct test *const t = ctx->t;
+	char path[PATH_MAX];
+	int ret, fd;
+
+	pthread_mutex_lock(&ctx->lock);
+	while (!ctx->start)
+		pthread_cond_wait(&ctx->cond, &ctx->lock);
+	pthread_mutex_unlock(&ctx->lock);
+
+	ret = snprintf(path, sizeof(path), "/dev/%s", t->dev);
+	if (ret < 0)
+		goto perr;
+	fd = open(path, flags);
+	if (fd == -1)
+		goto perr;
+	if (close(fd) == -1)
+		goto perr;
+	return (void *)EXIT_SUCCESS;
+perr:
+	perror(t->name);
+	return (void *)EXIT_FAILURE;
+}
+
+static void *reader(void *arg)
+{
+	return tester(arg, O_RDONLY);
+}
+
+static void *writer(void *arg)
+{
+	return tester(arg, O_WRONLY);
+}
 
 static void test(const struct test *restrict t)
 {
+	unsigned int nr = (t->readers+t->writers)*2;
+	const struct rlimit limit = {
+		.rlim_cur	= nr > 1024 ? nr : 1024,
+		.rlim_max	= nr > 1024 ? nr : 1024,
+	};
+	struct context ctx = {
+		.t	= t,
+		.lock	= PTHREAD_MUTEX_INITIALIZER,
+		.cond	= PTHREAD_COND_INITIALIZER,
+		.start	= 0,
+	};
+	pthread_t readers[t->readers], writers[t->writers];
+	char path[PATH_MAX];
+	int i, ret, err;
+
+	ret = snprintf(path, sizeof(path), "/dev/%s", t->dev);
+	if (ret < 0)
+		goto perr;
+	ret = setrlimit(RLIMIT_NOFILE, &limit);
+	if (ret == -1)
+		goto perr;
+	for (i = 0; i < t->readers; i++) {
+		err = pthread_create(&readers[i], NULL, reader, &ctx);
+		if (err) {
+			errno = err;
+			goto perr;
+		}
+	}
+	for (i = 0; i < t->writers; i++) {
+		err = pthread_create(&writers[i], NULL, writer, &ctx);
+		if (err) {
+			errno = err;
+			goto perr;
+		}
+	}
+	err = pthread_mutex_lock(&ctx.lock);
+	if (err) {
+		errno = err;
+		goto perr;
+	}
+	ctx.start = 1;
+	err = pthread_cond_broadcast(&ctx.cond);
+	if (err) {
+		errno = err;
+		goto perr;
+	}
+	err = pthread_mutex_unlock(&ctx.lock);
+	if (err) {
+		errno = err;
+		goto perr;
+	}
+	for (i = 0; i < t->readers; i++) {
+		void *retp;
+		err = pthread_join(readers[i], &retp);
+		if (err) {
+			errno = err;
+			goto perr;
+		}
+		if (retp != (void *)EXIT_SUCCESS)
+			goto err;
+	}
+	for (i = 0; i < t->writers; i++) {
+		void *retp;
+		err = pthread_join(writers[i], &retp);
+		if (err) {
+			errno = err;
+			goto perr;
+		}
+		if (retp != (void *)EXIT_SUCCESS)
+			goto err;
+	}
 	exit(EXIT_SUCCESS);
+perr:
+	perror(t->name);
+err:
+	exit(EXIT_FAILURE);
 }
 
 int main(void)
 {
 	const struct test *t, tests[] = {
+		{
+			.name		= "1 reader and 1 writer on scullfifo0",
+			.dev		= "scullfifo0",
+			.readers	= 1,
+			.writers	= 1,
+			.rbufsiz	= 4096,
+			.wbufsiz	= 4096,
+			.bufsiz		= 4096,
+			.alloc		= 4096,
+		},
+		{
+			.name		= "32 readers and 32 writers on scullfifo1",
+			.dev		= "scullfifo1",
+			.readers	= 32,
+			.writers	= 32,
+			.rbufsiz	= 4096,
+			.wbufsiz	= 4096,
+			.bufsiz		= 4096,
+			.alloc		= 4096,
+		},
+		{
+			.name		= "64 readers and 64 writers on scullfifo0",
+			.dev		= "scullfifo0",
+			.readers	= 64,
+			.writers	= 64,
+			.rbufsiz	= 4096,
+			.wbufsiz	= 4096,
+			.bufsiz		= 4096,
+			.alloc		= 4096,
+		},
+		{
+			.name		= "256 readers and 256 writers on scullfifo1",
+			.dev		= "scullfifo1",
+			.readers	= 256,
+			.writers	= 256,
+			.rbufsiz	= 4096,
+			.wbufsiz	= 4096,
+			.bufsiz		= 4096,
+			.alloc		= 4096,
+		},
+		{
+			.name		= "1 reader and 1 writer on scullfifo0 with 1KB buffer",
+			.dev		= "scullfifo0",
+			.readers	= 1,
+			.writers	= 1,
+			.rbufsiz	= 1024,
+			.wbufsiz	= 1024,
+			.bufsiz		= 1024,
+			.alloc		= 4096,
+		},
+		{
+			.name		= "1 reader and 1 writer on scullfifo1 with 8KB buffer",
+			.dev		= "scullfifo1",
+			.readers	= 1,
+			.writers	= 1,
+			.rbufsiz	= 8192,
+			.wbufsiz	= 8192,
+			.bufsiz		= 8192,
+			.alloc		= 8192,
+		},
+		{
+			.name		= "1 reader and 2 writers on scullfifo0 with 1KB buffer",
+			.dev		= "scullfifo0",
+			.readers	= 1,
+			.writers	= 2,
+			.rbufsiz	= 1024,
+			.wbufsiz	= 512,
+			.bufsiz		= 1024,
+			.alloc		= 4096,
+		},
+		{
+			.name		= "1 reader and 2 writers on scullfifo1 with 8KB buffer",
+			.dev		= "scullfifo1",
+			.readers	= 1,
+			.writers	= 2,
+			.rbufsiz	= 8192,
+			.wbufsiz	= 4096,
+			.bufsiz		= 8192,
+			.alloc		= 8192,
+		},
+		{
+			.name		= "2 readers and 1 writer on scullfifo0 with 1KB buffer",
+			.dev		= "scullfifo0",
+			.readers	= 2,
+			.writers	= 1,
+			.rbufsiz	= 512,
+			.wbufsiz	= 1024,
+			.bufsiz		= 1024,
+			.alloc		= 4096,
+		},
+		{
+			.name		= "2 readers and 1 writer on scullfifo1 with 8KB buffer",
+			.dev		= "scullfifo1",
+			.readers	= 2,
+			.writers	= 1,
+			.rbufsiz	= 4096,
+			.wbufsiz	= 8192,
+			.bufsiz		= 8192,
+			.alloc		= 8192,
+		},
 		{.name = NULL}, /* sentry */
 	};
 
