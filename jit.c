eaf264e69c4750c2a3bce45ea49c7b392f5c9242
Kei Nohguchi <kei@nohguchi.com>
2019-06-17 15:52:26 -0700

    jit: Add /proc/driver/jit/currenttime proc entry

--- jit.c
+++ jit.c
@@ -5,15 +5,51 @@
 #include <linux/kernel.h>
 #include <linux/device.h>
 #include <linux/proc_fs.h>
+#include <linux/seq_file.h>
+#include <linux/jiffies.h>
+#include <linux/time.h>
+#include <linux/timekeeping.h>
 
 static struct jit_driver {
 	struct proc_dir_entry	*top;
 	struct device_driver	base;
+	struct seq_operations	sops[1];
 } jit_driver = {
 	.top		= NULL,
 	.base.owner	= THIS_MODULE,
 	.base.name	= "jit",
 };
+
+static void *start_currenttime(struct seq_file *m, loff_t *pos)
+{
+	return PDE_DATA(file_inode(m->file));
+}
+
+static void stop_currenttime(struct seq_file *m, void *v)
+{
+	return;
+}
+
+static void *next_currenttime(struct seq_file *m, void *v, loff_t *pos)
+{
+	return m->private;
+}
+
+static int show_currenttime(struct seq_file *m, void *v)
+{
+	struct timespec64 ts;
+	u64 ns;
+
+	ktime_get_real_ts64(&ts);
+	ns = ktime_get_real_fast_ns();
+	seq_printf(m, "0x%08lx 0x%016llx %10lld.%09ld (ktime_get_real_ts64)\n"
+		   "%29c %10lld.%09lld (ktime_get_real_fast_ns)\n",
+		   jiffies&0xffffffff, get_jiffies_64(),
+		   ts.tv_sec, ts.tv_nsec,
+		   ' ',
+		   ns/NSEC_PER_SEC, ns%NSEC_PER_SEC);
+	return 0;
+}
 
 static int __init init(void)
 {
@@ -29,7 +65,19 @@ static int __init init(void)
 	if (IS_ERR(dir))
 		return PTR_ERR(dir);
 	drv->top = dir;
+	drv->sops[0].start	= start_currenttime;
+	drv->sops[0].stop	= stop_currenttime;
+	drv->sops[0].next	= next_currenttime;
+	drv->sops[0].show	= show_currenttime;
+	dir = proc_create_seq_data("currenttime", 0, drv->top, &drv->sops[0], drv);
+	if (IS_ERR(dir)) {
+		err = PTR_ERR(dir);
+		goto err;
+	}
 	return 0;
+err:
+	proc_remove(drv->top);
+	return err;
 }
 module_init(init);
 
--- tests/jit_test.c
+++ tests/jit_test.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <limits.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
@@ -9,16 +10,42 @@
 
 struct test {
 	const char	*const name;
+	const char	*const file;
 };
 
 static int test(const struct test *restrict t)
 {
+	char path[PATH_MAX];
+	char buf[512];
+	FILE *fp;
+	int ret;
+
+	ret = snprintf(path, sizeof(path), "/proc/driver/jit/%s", t->file);
+	if (ret < 0)
+		goto perr;
+	fp = fopen(path, "r");
+	if (!fp)
+		goto perr;
+	ret = fread(buf, sizeof(buf), 1, fp);
+	if (ret == 0 && ferror(fp))
+		goto perr;
+	if (fclose(fp) == -1)
+		goto perr;
+	buf[sizeof(buf)-1] = '\0';
+	fprintf(stdout, "%s:\n%s\n", t->name, buf);
 	exit(EXIT_SUCCESS);
+perr:
+	perror(t->name);
+	exit(EXIT_FAILURE);
 }
 
 int main(void)
 {
 	const struct test *t, tests[] = {
+		{
+			.name	= "/proc/driver/jit/currenttime file",
+			.file	= "currenttime",
+		},
 		{.name = NULL},
 	};
 
