e1bfae2320e0e63b4a71f446a8cce9037cad03c7
Kei Nohguchi <kei@nohguchi.com>
2019-10-01 17:03:22 -0700

    snull: Add sysfs device file check test

--- tests/snull_test.c
+++ tests/snull_test.c
@@ -1,7 +1,86 @@
 /* SPDX-License-Identifier: GPL-2.0 */
 #include <stdio.h>
+#include <string.h>
+#include <limits.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include "kselftest.h"
+
+struct test {
+	const char	*const name;
+};
+
+static void test(const struct test *const t)
+{
+	char path[PATH_MAX];
+	FILE *fp;
+	int ret;
+
+	ret = snprintf(path, sizeof(path), "/sys/class/net/sn0");
+	if (ret < 0)
+		goto perr;
+	fp = fopen(path, "r");
+	if (!fp)
+		goto perr;
+	if (fclose(fp) == -1)
+		goto perr;
+	ret = snprintf(path, sizeof(path), "/sys/class/net/sn1");
+	if (ret < 0)
+		goto perr;
+	fp = fopen(path, "r");
+	if (!fp)
+		goto perr;
+	if (fclose(fp) == -1)
+		goto perr;
+	exit(EXIT_SUCCESS);
+perr:
+	perror(t->name);
+	exit(EXIT_FAILURE);
+}
 
 int main(void)
 {
-	return 0;
+	const struct test *t, tests[] = {
+		{
+			.name	= "simple device file check",
+		},
+		{.name = NULL},
+	};
+
+	for (t = tests; t->name; t++) {
+		int ret, status;
+		pid_t pid;
+
+		pid = fork();
+		if (pid == -1)
+			goto perr;
+		else if (pid == 0)
+			test(t);
+
+		ret = waitpid(pid, &status, 0);
+		if (ret == -1)
+			goto perr;
+		if (WIFSIGNALED(status)) {
+			fprintf(stderr, "%s: signaled by %s\n",
+				t->name, strsignal(WTERMSIG(status)));
+			goto err;
+		}
+		if (!WIFEXITED(status)) {
+			fprintf(stderr, "%s: does not exit\n",
+				t->name);
+			goto err;
+		}
+		if (WEXITSTATUS(status))
+			goto err;
+		ksft_inc_pass_cnt();
+		continue;
+perr:
+		perror(t->name);
+err:
+		ksft_inc_fail_cnt();
+	}
+	if (ksft_get_fail_cnt())
+		ksft_exit_fail();
+	ksft_exit_pass();
 }
